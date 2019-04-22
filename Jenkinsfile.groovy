f38663cd74910311429ac222a83cfaafcfbab768
karakyrt <akiseleichuk84@gmail.com>
2019-04-22 14:52:32 -0500

    changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,21 +8,21 @@ pipeline{
         }
         stage("Initialization"){
             steps{
-                ws("${workspace}/nexus_jenkins/"){
+                ws("${workspace}/nexus_terraform/nexus_jenkins/"){
                     sh "terraform init"
                 }
             }
         }
         stage("Plan"){
             steps{
-                ws("${workspace}/nexus_jenkins/"){
+                ws("${workspace}/nexus_terraform/nexus_jenkins/"){
                     sh "terraform plan"
                 }  
             }
         }
         stage("Apply"){
             steps{
-                ws("${workspace}/nexus_jenkins"){
+                ws("${workspace}/nexus_terraform/nexus_jenkins"){
                     sh "terraform apply -auto-approve"
                 }
             }
