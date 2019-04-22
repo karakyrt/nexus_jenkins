143a5568d9761f4cbc356bcfb649726b4c1f3f70
karakyrt <akiseleichuk84@gmail.com>
2019-04-22 16:06:14 -0500

    changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     properties([parameters([string(defaultValue: 'Apply', description: 'What parameters you want to use??', name: 'Terraform_Plan_Apply_Destroy', trim: true)])])
-    stage("git pull"){
+    stage("git clone"){
         sh  'https://github.com/karakyrt/nexus_jenkins.git'
     }
     stage("Terraform init"){
